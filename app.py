548aa389edf9f7bc4b6cb0db6dc637f953b22793
Elmer Raúl Escandón Tufino <64042282+elmerescandon@users.noreply.github.com>
2024-08-30 12:33:49 -0500

    :sparkles: Show results

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ def load_test_data():
     return pd.read_csv("./data/test.csv")
 
 @st.cache_data
-def load_results():
-    return pd.read_csv("./results/results.csv")
+def load_results(file_path):
+    return pd.read_csv(file_path)
 
 df = load_test_data()
-results = load_results()
+results = load_results("./results/results.csv")
+results_2 = load_results("./results/results_wo_data.csv")
 
 # Overview
 with st.expander("Overview"):
@@ -22,7 +23,7 @@ with st.expander("Overview"):
 
 # Data Exploration
 with st.expander("Data Exploration"):
-    st.write("The data included 20,758 entries with 17 features with 8 numerical and 8 categorical. The target variable is 'CVRisk'.")
+    st.write("The data included 20,758 entries with 17 features with 8 numerical and 8 categorical. The target variable is 'CVRisk'. [1]")
 
 # Data Preprocessing
 with st.expander("Data Preprocessing"):
@@ -36,10 +37,19 @@ with st.expander("Data Preprocessing"):
                 4) Splitting the data into training and testing sets.
                 """)
 
+with st.expander("Feature Importance"):
+    st.write('According to [4], the dataset include more relevant information for the prediction of the CVR risk, such as:')
+    st.markdown(""" 
+                1) Physical Activity
+                2) Alcohol consumption
+                3) Use of technology devices
+                4) Frequency of high-caloric food consumption
+                5) Frequency of vegetables consumption"""); 
+    st.write('An evaluation considering the chosen classification models was performed to determine the feature importance.');
 
 # Model Selection
 with st.expander("Model Selection"):
-    st.write('_**Decision Tree**_')
+    st.write('_**Decision Tree [2]**_')
     st.write('1) Dataset contain both numerical and categorical variables.')
     st.write('2) Easy to interpret and visualize, so a feature importance can be extracted.')
     st.write('3) Features present a non-linear relationship with the target variable.')
@@ -55,13 +65,17 @@ with st.expander("Model Selection"):
     st.write('2) Gradient Boosting is an ensemble method that builds trees sequentially.')
     st.write('3) Gradient Boosting is robust to overfitting and can handle high-dimensional data.')
     
+    st.info('_Other vanilla models were tested but did not perform as well as the ensemble methods. See "data_models.ipynb" for more details._')
+
 # Results
 with st.expander("Results"):
     st.write('The results of the model evaluation are shown in the table below:')
     st.write(results)
+    st.write(results_2)
 
 # References
 with st.expander("References"):
     st.write('[1] F. M. Palechor and A. De la Hoz Manotas, "Dataset for estimation of obesity levels based on eating habits and physical condition in individuals from Colombia, Peru and Mexico," Data in Brief, vol. 25, p. 104344, 2019.')
     st.write('[2] De-La-Hoz-Correa, E., Mendoza Palechor, F., De-La-Hoz-Manotas, A., Morales Ortega, R., and Sánchez Hernández, A. B., "Obesity level estimation software based on decision trees," Universidad de la Costa, 2019.')
     st.write('[3] F. H. Yagin, M. Gülü, Y. Gormez, A. Castañeda-Babarro, C. Colak, G. Greco, F. Fischetti, and S. Cataldi, "Estimation of Obesity Levels with a Trained Neural Network Approach optimized by the Bayesian Technique," Applied Sciences, vol. 13, no. 6, Art. no. 3875, 2023. [Online].')
+    st.write('[4] W. Reade and A. Chow, "Multi-Class Prediction of Obesity Risk," Kaggle, 2024. [Online]. Available: https://kaggle.com/competitions/playground-series-s4e2')
